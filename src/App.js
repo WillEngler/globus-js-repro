e9595bc2f39978804be5f52b7c9a17ee813ad3be
Will Engler <Engler.Will@gmail.com>
2023-11-01 10:15:07 -0500

    try to add globus library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { authorization } from "@globus/sdk"
 
 function App() {
+  console.log(authorization);
+
   return (
     <div className="App">
       <header className="App-header">
